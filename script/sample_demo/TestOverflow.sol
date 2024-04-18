1cf9012a8e456db2d0c987ea4f97b53163e883fa
AlanYin <1838083144@qq.com>
2024-04-18 22:49:40 +0800

    feat:新增Int和地址类型代码

---
+++ script/sample_demo/TestOverflow.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSE
+pragma solidity >0.8.0;
+
+contract TestOverflow {
+    function add1() public pure returns (uint8) {
+        uint8 x = 128;
+        uint8 y = x * 2;
+        return y;
+    }
+
+    function add2() pure public returns (uint8) {
+        uint8 x = 250;
+        uint8 y =  6;
+        return x + y;
+    }
+}
